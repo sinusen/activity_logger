8188625fa3494a78c0c4196dab15e983e70620ec
sinusen <sen_sinu@yahoo.com.au>
2021-04-23 15:48:51 +1000

    Development of editable components

--- client/src/components/ActivityDisplay.js
+++ client/src/components/ActivityDisplay.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TableDisplay from "./TableDisplay";
 
 const ActivityDisplay = ({ activityData }) => {
+  const [editableActivityData, setEditableActivityData] = useState([]);
+  useEffect(() => {
+    if (activityData && activityData.length > 0) {
+      setEditableActivityData(
+        activityData.map((entry) => {
+          entry.editContent = false;
+          return entry;
+        })
+      );
+    }
+  }, [activityData]);
+
+  function editHandler(id) {
+    setEditableActivityData(
+      editableActivityData.map((entry) => {
+        entry.editContent = entry.pk === id ? true : false;
+        return entry;
+      })
+    );
+    console.log(editableActivityData);
+  }
+
+  function editSubmitHandler(id) {
+    console.log(editableActivityData);
+  }
+
   return (
     <div>
-      <TableDisplay activityData={activityData} />
+      <TableDisplay
+        activityData={editableActivityData}
+        editHandler={editHandler}
+        editSubmitHandler={editSubmitHandler}
+      />
     </div>
   );
 };
--- client/src/components/TableDisplay.js
+++ client/src/components/TableDisplay.js
@@ -1,23 +1,53 @@
 import React from "react";
 import { getSlashedDate, getFormattedTime } from "../helpers/time-functions";
 
-const TableDisplay = ({ activityData }) => {
+import "./TableDisplay.css";
+
+const TableDisplay = ({ activityData, editHandler, editSubmitHandler }) => {
   const renderTableRows = () => {
     if (activityData.length <= 0) {
       return null;
     }
     return activityData.map((row, index) => {
+      console.log(activityData);
       return (
-        <React.Fragment key={index}>
+        <React.Fragment key={row.pk}>
           <tr>
-            <th scope="row">{index + 1}</th>
-            <td>{getSlashedDate(Number(row.epoch_ms))}</td>
-            <td>{getFormattedTime(Number(row.epoch_ms))}</td>
+            <td contentEditable={row.editContent}>
+              {getSlashedDate(Number(row.epoch_ms))}
+            </td>
+            <td contentEditable={row.editContent}>
+              {getFormattedTime(Number(row.epoch_ms))}
+            </td>
             <td>{row.machine_name}</td>
             <td>{row.initials}</td>
+            <td>
+              {row.editContent ? (
+                <React.Fragment>
+                  <button
+                    onClick={editSubmitHandler.bind(null, row.pk)}
+                    className="btn btn-primary btn-sm"
+                  >
+                    Submit
+                  </button>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <button
+                    onClick={editHandler.bind(null, row.pk)}
+                    className="edit-button-group btn btn-sm btn-warning"
+                  >
+                    Edit log
+                  </button>
+                  <button className="btn btn-sm btn-danger">Delete log</button>
+                </React.Fragment>
+              )}
+            </td>
           </tr>
           <tr>
-            <td colSpan="5">{row.activity}</td>
+            <td colSpan="5" contentEditable={row.editContent}>
+              {row.activity}
+            </td>
           </tr>
         </React.Fragment>
       );
@@ -28,11 +58,11 @@ const TableDisplay = ({ activityData }) => {
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">Sl No</th>
             <th scope="col">Date</th>
             <th scope="col">Time</th>
             <th scope="col">Machine</th>
             <th scope="col">Operator</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>{renderTableRows()}</tbody>
